51e1a529c06dd8776012ca63eddb03e0c9a1425a
ZHENK <chengzhenyang@gmail.com>
2020-10-29 20:44:41 +1300

    feat - Added working implementation of task 1 Introduced Reflector class with methods getPublicField(), getPublicMethod(), executeMethod() to a qualified classname from constructor using Java Reflection API.
    Introduced T1Main class with main loops to interact with Reflector class with features defined by task 1. These feat includes asking for classname, printing public fields and methods, executing methods (without params).

--- src/se254/a4/t1/Reflector.java
+++ src/se254/a4/t1/Reflector.java
@@ -1,18 +1,72 @@
 package se254.a4.t1;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * SE254 Assignment 4 Task 1 Reflector - Instructions
- *  
- * This class should be implemented to contain the reflection functionality described in the
- * assignment hand-out. 
- * This class must be called by T1Main for executing the desired reflection for a given class e.g. class Counter. * 
- * All changes required for Task 1 must be performed on a new branch named "T1Branch". 
- * You can create this new branch from our IDE.
  * 
- * @author Author Name: [YOUR NAME] Author UPI: [YOUR UPI]
- * @version Date: [CURRENT DATE] 
+ * This class should be implemented to contain the reflection functionality
+ * described in the assignment hand-out. This class must be called by T1Main for
+ * executing the desired reflection for a given class e.g. class Counter. * All
+ * changes required for Task 1 must be performed on a new branch named
+ * "T1Branch". You can create this new branch from our IDE.
+ * 
+ * @author Author Name: Cheng-Zhen Yang Author UPI: cyan562
+ * @version Date: 29/10/2020
  */
 
 public class Reflector {
+    private Object _object;
+    private Class<?> _class;
 
+    public Reflector(String classname) throws ClassNotFoundException {
+        _class = Class.forName(classname);
+
+        try {
+            Constructor<?> constructor = _class.getConstructor();
+            _object = constructor.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ClassNotFoundException();
+        }
+    }
+
+    public List<String> getPublicField() {
+        List<String> fields = new ArrayList<String>();
+
+        for (Field f : _class.getDeclaredFields()) {
+            try {
+                if (Modifier.isPublic(f.getModifiers())) {
+                    fields.add(f.getName() + ": " + f.get(_object));
+                }
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return fields;
+    }
+
+    public List<String> getPublicMethod() {
+        List<String> methods = new ArrayList<String>();
+
+        for (Method m : _class.getDeclaredMethods()) {
+            try {
+                if (m.getParameterTypes().length == 0 && Modifier.isPublic(m.getModifiers())) {
+                    methods.add(m.getName() + "()");
+                }
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
+        return methods;
+    }
+
+    public void executeMethod(String method) throws Exception {
+        _class.getMethod(method).invoke(_object);
+    }
 }
--- src/se254/a4/t1/T1Main.java
+++ src/se254/a4/t1/T1Main.java
@@ -1,28 +1,86 @@
 package se254.a4.t1;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
- * SE254 Assignment 4 Task 1 Main - Instructions 
+ * SE254 Assignment 4 Task 1 Main - Instructions
  * 
- * This class should be implemented to accept the user input 
- * through args[] and delegate the reflection functionality to class Reflector. 
+ * This class should be implemented to accept the user input through args[] and
+ * delegate the reflection functionality to class Reflector.
  * 
- * You may implement additional classes if you wish, but this class should be the main program entry
- * point.
+ * You may implement additional classes if you wish, but this class should be
+ * the main program entry point.
  * 
- * You must STAGE, COMMIT and PUSH your changes everytime you are 
- * "done" (see the "definition of done" in Agile/Scrum/XP) with writing/changing this class, i.e. 
- * after successfully completing Task 1, Task 2 and Task 3, respectively
- * You may COMMIT and PUSH prematurely too i.e. before each task is completed, but 
- * you "must" COMMIT and PUSH to the remote repo after finishing each of the A4 tasks.
+ * You must STAGE, COMMIT and PUSH your changes everytime you are "done" (see
+ * the "definition of done" in Agile/Scrum/XP) with writing/changing this class,
+ * i.e. after successfully completing Task 1, Task 2 and Task 3, respectively
+ * You may COMMIT and PUSH prematurely too i.e. before each task is completed,
+ * but you "must" COMMIT and PUSH to the remote repo after finishing each of the
+ * A4 tasks.
  * 
- * @author Author Name: [YOUR NAME] Author UPI: [YOUR UPI]
- * @version Date: [CURRENT DATE] 
+ * @author Author Name: Cheng-Zhen Yang Author UPI: cyan562
+ * @version Date: 29/10/2020
  */
 
 public class T1Main {
-	public static void main(String[] args) {
+	private static Reflector reflector;
+    private static Scanner scan = new Scanner(System.in);
 
-		// TODO Assignment 4, Question 1-2.	
+    public static void main(String[] args) {
+        String classname;
 
-	}
+        if (args.length == 1) {
+            classname = args[0];
+        }
+
+        while (true) {
+            System.out.print("\nEnter a fully qualified class name: ");
+            classname = getInput();
+
+            try {
+                reflector = new Reflector(classname);
+                break;
+            } catch (Exception e) {
+                System.out.printf("\nNo such fully qualified classname: %s (ctrl-c to quit)\n", classname);
+            }
+        }
+
+        System.out.printf("\nAn object has been created for %s\n", classname);
+
+        while (true) {
+            System.out.println("\nPublic fields:");
+            printList(reflector.getPublicField());
+
+            System.out.println("\nPublic methods:");
+            printList(reflector.getPublicMethod());
+
+            while (true) {
+                System.out.print("\nEnter the name of a method to execute:");
+                String methodName = getInput();
+                try {
+                    reflector.executeMethod(methodName.replace("()", ""));
+                    break;
+                } catch (Exception e) {
+                    System.out.printf("\nNo such method: %s (ctrl-c to quit)\n", methodName);
+                }
+            }
+        }
+    }
+
+    private static String getInput() {
+        try {
+            String s = scan.next();
+            return s.trim();
+        } catch (NoSuchElementException e) {
+            return "";
+        }
+    }
+
+    private static void printList(List<String> list) {
+        for (String line : list) {
+            System.out.println("  " + line);
+        }
+    }
 }
